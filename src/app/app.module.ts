b2bdadba3a189a49ba4d5e788ccce3238c4bd6a5
Jose Zamudio <jzamudio@popsugar.com>
2018-05-14 20:19:43 -0500

    use title service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { DragScrollModule } from 'ngx-drag-scroll';
 
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, Title } from '@angular/platform-browser';
 import { NgxsModule } from '@ngxs/store';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
@@ -10,6 +10,8 @@ import { HomeComponent } from './home/home.component';
 import { MaterialModule } from './material/material.module';
 import { RestaurantComponent } from './restaurant/restaurant.component';
 import { RestaurantInfoComponent } from './restaurant/shared/info/info.component';
+import { MenuItemComponent } from './restaurant/shared/menu/menu-item/menu-item.component';
+import { MenuSectionComponent } from './restaurant/shared/menu/menu-section/menu-section.component';
 import { SearchComponent } from './search/search.component';
 import { CardSliderComponent } from './shared/components/card-slider/card-slider.component';
 import { HeaderComponent } from './shared/components/header/header.component';
@@ -19,8 +21,6 @@ import { SearchBarComponent } from './shared/components/search-bar/search-bar.co
 import { SectionHeaderComponent } from './shared/components/section-header/section-header.component';
 import { MockService } from './shared/services/mock/mock.service';
 import { ScrollerService } from './shared/services/scroller/scroller.service';
-import { MenuSectionComponent } from './restaurant/shared/menu/menu-section/menu-section.component';
-import { MenuItemComponent } from './restaurant/shared/menu/menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
@@ -45,7 +45,7 @@ import { MenuItemComponent } from './restaurant/shared/menu/menu-item/menu-item.
     AppRoutingModule,
     NgxsModule.forRoot([])
   ],
-  providers: [ScrollerService, MockService],
+  providers: [ScrollerService, MockService, Title],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/restaurant/restaurant.component.ts
+++ src/app/restaurant/restaurant.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
 
 import { ItemCard, MockService } from './../shared/services/mock/mock.service';
@@ -13,9 +14,11 @@ export class RestaurantComponent {
   restaurant: ItemCard;
   menuCategories: Array<string>;
 
-  constructor(private actiavedRoute: ActivatedRoute, private mock: MockService) {
+  constructor(private actiavedRoute: ActivatedRoute, private mock: MockService, private titleService: Title) {
     this.restaurantId = this.actiavedRoute.snapshot.params.id;
     this.restaurant = this.mock.getStoreById(this.restaurantId);
+
+    this.titleService.setTitle(`${this.restaurant.title} • ${this.restaurant.city} • Eatza`);
   }
 
   getMenuCategories(): Array<string> {
